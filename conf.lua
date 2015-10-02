03f0446ad3f18d6f041958a887e7aac016879ec9
Ricky Thomson <flytheflag@gmx.co.uk>
2015-10-02 14:21:09 +0100

    engine version/name

--- conf.lua
+++ conf.lua
@@ -1,6 +1,9 @@
+name = "platform"
+version = 0.1
+
 function love.conf(t)
 	t.version = "0.9.2"
-	t.window.title = "Platform 0.1"
+	t.window.title = name .. " " .. version
 	t.window.width = 800
 	t.window.height = 600
 	t.modules.joystick = false
