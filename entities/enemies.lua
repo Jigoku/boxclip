9964f529fe844ee72bb35e9b4c69c6b84f1c1530
Ricky Thomson <flytheflag@gmx.co.uk>
2015-10-21 19:19:32 +0100

    add license info

--- entities/enemies.lua
+++ entities/enemies.lua
@@ -1,4 +1,19 @@
-enemies = {}
+--[[
+ * Copyright (C) 2015 Ricky K. Thomson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * u should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --]]
+ 
+ enemies = {}
 
 walker = love.graphics.newImage( "graphics/enemies/walker.png")
 floater = love.graphics.newImage( "graphics/enemies/floater.png")
--- physics.lua
+++ physics.lua
@@ -1,5 +1,19 @@
-physics = {}
+--[[
+ * Copyright (C) 2015 Ricky K. Thomson
+ * 
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ * u should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ --]]
 
+physics = {}
 
 function physics:applyVelocity(object, dt) 
 	if object.alive == 1 then
