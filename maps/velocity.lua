b454fbf1b6e25c3fd3c466e2639e556cf2822619
Ricky Thomson <flytheflag@gmx.co.uk>
2016-10-08 11:52:27 +0100

    update test maps

---
+++ maps/velocity.lua
@@ -0,0 +1,11 @@
+world.mapmusic = 0
+world.mapambient = 0
+world.maptitle = "unnamed map"
+world.nextmap = "title"
+world:settheme("frost")
+platforms:add(-1010,-9720,2410,640,1,0,0,0,0,0,0,1)
+platforms:add(1400,-9660,3820,850,1,0,0,0,0,0,0,1)
+platforms:add(5220,-9580,3820,850,1,0,0,0,0,0,0,1)
+platforms:add(9010,-9470,3820,850,1,0,0,0,0,0,0,1)
+springs:add(-960,-9760,2,"spring_l")
+portals:add(-860,-9800,"spawn")
