2eb87c02d282f75ad2e87db97d8a072b2afd957b
Ricky Thomson <flytheflag@gmx.co.uk>
2020-10-27 08:13:43 +0000

    add impact sound for crush, when colliding with platform

--- src/entities/enemies/crusher.lua
+++ src/entities/enemies/crusher.lua
@@ -1,6 +1,6 @@
 --[[
  * Copyright (C) 2015 - 2018 Ricky K. Thomson
- * 
+ *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
@@ -12,7 +12,7 @@
  * u should have received a copy of the GNU General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  --]]
- 
+
 crusher = {}
 table.insert(enemies.list, "crusher")
 table.insert(editor.entities, {"crusher", "enemy"})
@@ -20,16 +20,16 @@ enemies.textures["crusher"] = {love.graphics.newImage("data/images/enemies/crush
 
 
 function crusher:worldInsert(x,y,movespeed,movedist,dir,name)
-	
+
 	table.insert(world.entities.enemy, {
 		movespeed = movespeed or 100,
 		movedist = movedist or 300,
 		movey = 1,
-		ticks = love.math.random(100), 
-		x = x or 0, 
-		y = y or 0, 
-		xorigin = x, 
-		yorigin = y, 
+		ticks = love.math.random(100),
+		x = x or 0,
+		y = y or 0,
+		xorigin = x,
+		yorigin = y,
 		w = enemies.textures[name][1]:getWidth(),
 		h = enemies.textures[name][1]:getHeight() ,
 		group = "enemy",
@@ -44,27 +44,22 @@ end
 function crusher:checkCollision(object, dt)
 	object.ticks = object.ticks +1
 	self:update(object, dt)
-	
-	
-	-- NOT ACTIVE WHILST EDITING 
+
+	-- NOT ACTIVE WHILST EDITING
 	if mode == "game" and player.alive and collision:check(player.newX,player.newY,player.w,player.h,
 		object.x+5,object.y+5,object.w-10,object.h-10) then
-		
+
 		if(object.y < player.y and (player.x > object.x and (player.x + player.w) < (object.x + object.w))) then
-			
 			player:die(object.type)
-		
-		elseif (object.y < player.y and (player.x<=object.x or (player.x + player.w) >= (object.x + object.w))) then 
-			
+
+		elseif (object.y < player.y and (player.x<=object.x or (player.x + player.w) >= (object.x + object.w))) then
+
 			subtract = player.dir * player.speed * 1.3 * 0.005;
-			player.xvel = 0 
+			player.xvel = 0
 			player.x = player.x - subtract
 			player.newX = player.x
-			
 		end
-		
 	end
-	
 end
 
 
@@ -83,6 +78,17 @@ function crusher:update(object, dt)
 		if object.y > object.yorigin + object.movedist then
 			object.y = object.yorigin + object.movedist
 			object.movespeed = -object.movespeed
+
+			if world:inview(object) then
+				for i, platform in ipairs(world.entities.platform) do
+					if world:inview(platform) then
+						if collision:top(object,platform) then
+							sound:play(sound.effects["crush"])
+						end
+					end
+				end
+			end
+
 		end
 		if object.y < object.yorigin  then
 			object.y = object.yorigin
@@ -97,26 +103,26 @@ function crusher:update(object, dt)
 
 		object.newY = object.y + mv_speed * dt
 	end
-	
+
 	physics:update(object)
-	
+
 end
 
 
 function crusher:drawdebug(enemy, i)
-	
+
 	--bounds
 	love.graphics.setColor(1,0,0,1)
 	love.graphics.rectangle("line", enemy.x+5, enemy.y+5, enemy.w-10, enemy.h-10)
-	
+
 	--hitbox
 	love.graphics.setColor(1,0.78,0.39,1)
 	love.graphics.rectangle("line", enemy.x, enemy.y, enemy.w, enemy.h)
-	
+
 	local texture = enemies.textures[enemy.type]
 	love.graphics.setColor(1,0,1,0.19)
 	love.graphics.rectangle("fill", enemy.x, enemy.yorigin, texture[(enemy.frame or 1)]:getWidth(), enemy.movedist + texture[(enemy.frame or 1)]:getHeight())
 	love.graphics.setColor(1,0,1,1)
 	love.graphics.rectangle("line", enemy.x, enemy.yorigin, texture[(enemy.frame or 1)]:getWidth(), enemy.movedist + texture[(enemy.frame or 1)]:getHeight())
-	
+
 end
